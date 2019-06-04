dcd4b604cfe53a94122e29ffa6b916373d6f8259
Dustin Heisey <dustinheisey@Dustins-MBP.lan>
2019-06-04 16:36:04 -0700

    fix copywrite

--- src/components/layout/footer/Copywrite.jsx
+++ src/components/layout/footer/Copywrite.jsx
@@ -14,31 +14,36 @@ const StyledContainer = styled.div`
 
   ${media.down('phone')`
     justify-content: center;
+    flex-direction: column;
   `}
 `
 
 export default () => (
   <StyledContainer>
-    <small style={{ display: 'inline' }}>
-      &copy; 2019&nbsp;
-    </small>
-    <small>
-      <Link hover='var(--primary-light)' dark to='/'>
-        Dustin Heisey.&nbsp;
-      </Link>
-    </small>
-    <small style={{ display: 'inline' }}>
-      Images by&nbsp;
-    </small>
-    <small>
-      <Link
-        external
-        dark
-        hover='var(--primary-light)'
-        to='https://www.kylekrempleyphoto.com'
-      >
-        Trinamic Studio LLC
-      </Link>
-    </small>
+    <div>
+      <small style={{ display: 'inline' }}>
+        &copy; 2019&nbsp;
+      </small>
+      <small>
+        <Link hover='var(--primary-light)' dark to='/'>
+          Dustin Heisey.&nbsp;
+        </Link>
+      </small>
+    </div>
+    <div>
+      <small style={{ display: 'inline' }}>
+        Images by&nbsp;
+      </small>
+      <small>
+        <Link
+          external
+          dark
+          hover='var(--primary-light)'
+          to='https://www.kylekrempleyphoto.com'
+        >
+          Trinamic Studio LLC
+        </Link>
+      </small>
+    </div>
   </StyledContainer>
 )
