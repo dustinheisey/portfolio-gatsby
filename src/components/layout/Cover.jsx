a6da076c0666b0cea83d17162d3377dd9f61ae59
Dustin Heisey <dustinheisey@Dustins-MBP.lan>
2019-06-10 12:21:25 -0700

    fix for now

--- src/components/layout/Cover.jsx
+++ src/components/layout/Cover.jsx
@@ -192,19 +192,24 @@ const Cover = ({
             <Coffee />
           </BackgroundImage>
           <Footer />
-          {index && (
+          {/* {index && (
             <script>
-              if(typeof window !== 'undefined' &&
-              window.netlifyIdentity)
-              {window.netlifyIdentity.on('init', user => {
-                if (!user) {
-                  window.netlifyIdentity.on('login', () => {
-                    document.location.href = '/admin/'
+              if(typeof window !== 'undefined') {
+                if(window.netlifyIdentity){
+                  window.netlifyIdentity.on('init', user => {
+                    if (!user) {
+                      window.netlifyIdentity.on('login', () => {
+                        document.location.href = '/admin/'
+                      })
+                    }
                   })
                 }
-              })}
+                else {}
+              }
+              else
+              {}
             </script>
-          )}
+          )} */}
         </>
       )
     }}
