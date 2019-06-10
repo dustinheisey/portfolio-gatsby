766d4db8f5a21f6b8cdecdf770e285148be89b0e
Dustin Heisey <dustinheisey@Dustins-MBP.lan>
2019-06-10 15:02:50 -0700

    test

--- src/components/layout/header/SEO.jsx
+++ src/components/layout/header/SEO.jsx
@@ -1,3 +1,10 @@
+/* eslint-disable prefer-rest-params */
+/* eslint-disable func-names */
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable no-multi-assign */
+/* eslint-disable no-undef */
+/* eslint-disable no-return-assign */
+/* eslint-disable eslint */
 import { React, Helmet } from '../../../../config/imports'
 
 export default ({ title, description }) => (
@@ -39,5 +46,19 @@ export default ({ title, description }) => (
     ]}
   >
     <html lang='en' />
+    <script>
+      !function(g,s,q,r,d)
+      {
+        (r = g[r] =
+          g[r] ||
+          function() {
+            ;(r.q = r.q || []).push(arguments)
+          })
+      }
+      d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+      d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
+      insertBefore(d,q)}(window,document,'script','_gs');
+      _gs('GSN-995552-V'); _gs('set', 'anonymizeIP', true);
+    </script>
   </Helmet>
 )
