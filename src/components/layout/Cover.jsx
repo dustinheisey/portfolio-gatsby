a0c4343bf80799372c71772b14a66109a69c0124
Dustin Heisey <dustinheisey@Dustins-MBP.lan>
2019-06-10 12:00:47 -0700

    test

--- src/components/layout/Cover.jsx
+++ src/components/layout/Cover.jsx
@@ -193,7 +193,8 @@ const Cover = ({
           <Footer />
           {index && (
             <script>
-              if (window.netlifyIdentity)
+              if(typeof window !== undefined &&
+              window.netlifyIdentity)
               {window.netlifyIdentity.on('init', user => {
                 if (!user) {
                   window.netlifyIdentity.on('login', () => {
